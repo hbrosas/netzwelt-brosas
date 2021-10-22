07983c1f13abce7348763569bbd0b55167ed1d5d
Hazel Brosas <hazelannebrosas@gmail.com>
2021-10-22 10:00:37 +0800

    update bootstrap files to cdn

--- resources/views/layout/_header.blade.php
+++ resources/views/layout/_header.blade.php
@@ -4,5 +4,4 @@
 <meta name="description" content="">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700">
-<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-<link rel="stylesheet" href="{{asset('css/bootstrap-grid.min.css')}}">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
